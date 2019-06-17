7468944ba8b48c156b05fd201876e626d6b5a821
wangshilin <wangshilin@021.com>
2019-06-17 16:43:21 +0800

    bug修改

--- config/dev.env.js
+++ config/dev.env.js
@@ -5,5 +5,5 @@ const prodEnv = require('./prod.env')
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   API_ROOT: '"http://test.dpqct.com"'
-  // API_ROOT: '"http://172.18.240.31:8080"'
+  // API_ROOT: '"https://fwqyd.dpqct.com"'
 })
