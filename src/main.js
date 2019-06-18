d62ea9ff4d008215ee9e46a23cad2e1c274dd0ce
wangshilin <wangshilin@021.com>
2019-06-18 14:52:47 +0800

    修改问题

--- src/main.js
+++ src/main.js
@@ -7,28 +7,11 @@ import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import './assets/css/main.css'
 import './assets/css/scrollbar.css'
-import NProgress from 'nprogress'
 import common from './assets/js/common'
 Vue.prototype.common = common
 Vue.use(ElementUI)
 
 Vue.config.productionTip = false
-
-router.beforeEach((to,from,next)=>{
-  NProgress.start()
-  var token = sessionStorage.getItem('token')
-  if(!token && to.name != 'login'){
-    app && app.$message.warning('未授权,请登录后继续')
-    NProgress.done()
-    return next({name: 'login'})
-  }
-  return next()
-})
-router.afterEach(transition => {
-  setTimeout(() => {
-    NProgress.done()
-  })
-})
 
 var app = new Vue({
   el: '#app',
